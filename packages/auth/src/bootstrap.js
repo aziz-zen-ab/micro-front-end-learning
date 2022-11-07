651f20c34325be52497ea3240f90b9c2e72d5ba7
Aziz Hussein <aziz@accelbyte.net>
2022-11-07 09:05:17 +0700

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,7 +4,7 @@ import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignin, onNavigate, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({
         initialEntries: [initialPath]
     });
@@ -15,7 +15,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 
     ReactDOM.render(
-        <App history={history} />,
+        <App history={history} onSignin={onSignin} />,
         el
     )
 
@@ -33,7 +33,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // if we are in dev and isolation
 // call mount immediately
 if(process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root')
+    const devRoot = document.querySelector('#_auth-dev-root')
 
     if(devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() })
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,33 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-import MarketingApp from "./component/MarketingApp";
+import Progress from "./component/Progress";
 import Header from './component/Header';
+
+const MarketingLazy = lazy(() => import('./component/MarketingApp'))
+const AuthLazy = lazy(() => import('./component/AuthApp'))
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 })
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false)
+
     return (
     <BrowserRouter>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <div>
-                <Header/>
+                <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn}/>
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth" >
+                            <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                        </Route>
+                        <Route path="/" component={MarketingLazy} />
+                    </Switch>
+                </Suspense>
                 <MarketingApp/>
             </div>
         </StylesProvider>
